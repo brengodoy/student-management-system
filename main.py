5ccd4dceee35f213589c4bddc52e1d0310717cd6
brengodoy <dia.godoybrenda@gmail.com>
2025-06-13 14:50:54 +0200

    created __str__ for Teacher and Grade

--- main.py
+++ main.py
@@ -64,19 +64,19 @@ class Teacher():
     def teaches_in_course(self,course):
         self.courses.append(course)
         course.teacher = self
-        
-    def show_teacher_data(self):
-        print("Teahcer's name: " + self.full_name)
-        print("Teahcer's id: " + self.teacher_id)
-        print("This teacher teaches in:")
-        for course in self.courses:
-            print(course.name)
+            
+    def __str__(self):
+        courses_str = "\n".join([course.name for course in self.courses])
+        return (f"Teacher's name: {self.full_name}\nTeacher's id: {self.teacher_id}\nThis teacher teaches in:\n{courses_str}")
 
 class Grade():
     def __init__(self,student,course,grade):
         self.student = student
         self.course = course
         self.grade = grade
+        
+    def __str__(self):
+        return (f"Student: {self.student.full_name}\nCourse: {self.course.name}\nGrade: {self.grade}")
 
 all_grades = []
 course1 = Course("Biology",101)
@@ -90,7 +90,7 @@ student1.enroll_in_course(course2)
 #print(" ")
 #student1.show_student_data()
 #print(student1)
-print(course1)
+#print(course1)
 grade1 = Grade(student1,course1,10)
 grade3 = Grade(student2,course1,9)
 grade2 = Grade(student1,course2,7)
@@ -100,3 +100,7 @@ all_grades.append(grade3)
 # student1.show_all_grades()
 # student1.show_average_grade()
 # course1.show_average_grade_course()
+teacher1 = Teacher("Ana Godoy", 987654321)
+teacher1.teaches_in_course(course1)
+#print(teacher1)
+print(grade1)
